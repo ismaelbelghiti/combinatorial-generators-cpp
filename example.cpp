59e30a800d99f7aafcf67b79fdb5d0be75ed0b29
Isma <isma@gmail.com>
2016-06-18 14:10:11 +0200

    add rand_spanning_tree with example

--- example.cpp
+++ example.cpp
@@ -25,20 +25,53 @@ void rand_parentheses_example() {
 	cout << endl << endl;
 }
 
+void display_edges(vector< pair<int,int> > edges) {
+	for (int i = 0; i < (int)edges.size(); i++) {
+		cout << "(" << edges[i].first << ","<< edges[i].second << ") ";
+	}
+}
+
 void rand_tree_example() {
 	cout << "rand_tree_example:" << endl;
 	int nbNodes = 6;
 	vector< pair<int,int> > edges = rand_tree(nbNodes);
-	for (int i = 0; i < (int)edges.size(); i++) {
-		cout << "(" << edges[i].first << ","<< edges[i].second << ") ";
-	}
+	display_edges(edges);
 	cout << endl << endl;
 }
 
-
+/*
+  This graph will be used as input for different examples.
+  The result is on the form (nbNodes, edgeList).
+*/
+pair<int, vector< pair<int,int> > > small_graph() {
+	int nbNodes = 8;
+	vector< pair<int,int> > edges;
+	edges.push_back(make_pair(0, 1));
+	edges.push_back(make_pair(0, 2));
+	edges.push_back(make_pair(1, 3));
+	edges.push_back(make_pair(2, 3));
+	edges.push_back(make_pair(3, 4));
+	edges.push_back(make_pair(4, 5));
+	edges.push_back(make_pair(4, 6));
+	edges.push_back(make_pair(5, 7));
+	edges.push_back(make_pair(6, 7));
+	return make_pair(nbNodes, edges);
+}
+	
+void rand_spanning_tree_example() {
+	cout << "rand_spanning_tree_example:" << endl;
+	pair<int, vector< pair<int,int> > > g = small_graph();
+	int nbNodes = g.first;
+	vector< pair<int,int> > edges = g.second;
+	vector< pair<int,int> > treeEdges = rand_spanning_tree(nbNodes, edges);
+	display_edges(treeEdges);
+	cout << endl << endl;
+}
+	
 int main() {
 	rand_perm_example();
 	rand_parentheses_example();
 	rand_tree_example();
+	rand_spanning_tree_example();
 	return 0;
 }
